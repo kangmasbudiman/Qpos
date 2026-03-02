332b493bf20ac7f0db0f2932ddf6561a99bc811e
kangmas <kraffitsolutino@gmail.com>
2026-03-02 13:32:27 +0700

    update harga

--- pos_offline_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pos_offline_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,6 +15,7 @@ import print_bluetooth_thermal
 import printing
 import share_plus
 import sqflite_darwin
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
@@ -27,4 +28,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
